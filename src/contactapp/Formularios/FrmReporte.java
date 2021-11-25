b56b5dd371d07e4061ce3fd333f11613adb406c6
Deivis <delahozdeivis123@gmail.com>
2021-11-25 12:00:55 -0500

    segundo reporte de datos globales agregado

--- src/contactapp/Formularios/FrmReporte.java
+++ src/contactapp/Formularios/FrmReporte.java
@@ -25,6 +25,8 @@ public class FrmReporte extends javax.swing.JFrame {
 
         jScrollPane1 = new javax.swing.JScrollPane();
         tblReporte1 = new javax.swing.JTable();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        tblReporte2 = new javax.swing.JTable();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("Reportes");
@@ -49,21 +51,38 @@ public class FrmReporte extends javax.swing.JFrame {
         ));
         jScrollPane1.setViewportView(tblReporte1);
 
+        tblReporte2.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane2.setViewportView(tblReporte2);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 630, Short.MAX_VALUE)
-                .addContainerGap())
+                .addGap(50, 50, 50)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 550, Short.MAX_VALUE)
+                    .addComponent(jScrollPane2))
+                .addGap(50, 50, 50))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 310, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(311, 311, 311))
+                .addGap(50, 50, 50)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 250, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 250, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(50, 50, 50))
         );
 
         pack();
@@ -77,8 +96,11 @@ public class FrmReporte extends javax.swing.JFrame {
 
     public void ListarReporte(){
         DefaultTableModel model = new DefaultTableModel();
+        DefaultTableModel model2 = new DefaultTableModel();
         String[] Indices = {"Nombre de usuario", "Cantidad de contactos"};
+        String[] Indices2 = {"Total usuarios", "Total contactos", "Total numeros", "Total correos", "Total direcciones"};
         model.setColumnIdentifiers(Indices);
+        model2.setColumnIdentifiers(Indices2);
         try{            
             stmt = reg.createStatement();
             ResultSet rs = stmt.executeQuery("CALL report_users_and_contacts();");
@@ -86,6 +108,12 @@ public class FrmReporte extends javax.swing.JFrame {
                 model.addRow(new Object[] {rs.getString("name") + " " + rs.getString("last_name"), rs.getInt("amount")});
             }
             tblReporte1.setModel(model);
+            rs.close();
+            rs = stmt.executeQuery("CALL amount_UCNEA();");
+            while(rs.next()){
+                model2.addRow(new Object[] {rs.getInt("users"), rs.getInt("contacts"), rs.getInt("numbers"), rs.getInt("emails"), rs.getInt("address")});
+            }
+            tblReporte2.setModel(model2);
         }catch(SQLException e){
             System.out.println(e);
         }
@@ -126,7 +154,9 @@ public class FrmReporte extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JTable tblReporte1;
+    private javax.swing.JTable tblReporte2;
     // End of variables declaration//GEN-END:variables
 
 }
