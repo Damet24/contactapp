4bf68dd006203aa09bd184c0b7d516769efb1cbf
Daniel Mercado <damet.2401@gmail.com>
2021-11-25 12:31:51 -0500

    Ajustes del buscador

--- src/contactapp/Formularios/FrmPrincipal.java
+++ src/contactapp/Formularios/FrmPrincipal.java
@@ -228,24 +228,29 @@ public class FrmPrincipal extends javax.swing.JFrame {
 
     private void btnBuscarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnBuscarActionPerformed
         try{
-            stmt = reg.createStatement();
-            ResultSet rs = stmt.executeQuery("CALL search_contact ('%" + txtBuscar.getText() + "%', " + usuario.getId() + ");");
-            LimpearUsuarios();
-            cont = 0;
-            while(rs.next()){
-                Statement stmt2 = reg.createStatement();
-                ResultSet rs2 = stmt2.executeQuery("SELECT * FROM `numbers` where fk_contact = " + rs.getString("id_contact") + ";");
-                if(rs2.next()){
-                    ShowPanel(new FrmContactoModelo(rs.getString("name") + " " + rs.getString("last_name"), rs2.getString("number"),rs.getString("id_contact"), rs2.getString("id_number"), usuario, this), 0, 100 * cont);
+            System.out.println("buscar: " + txtBuscar.getText());
+            if(!"".equals(txtBuscar.getText())){
+                stmt = reg.createStatement();
+                ResultSet rs = stmt.executeQuery("CALL search_contact ('%" + txtBuscar.getText() + "%', " + usuario.getId() + ");");
+                LimpearUsuarios();
+                cont = 0;
+                while(rs.next()){
+                    Statement stmt2 = reg.createStatement();
+                    ResultSet rs2 = stmt2.executeQuery("SELECT * FROM `numbers` where fk_contact = " + rs.getString("id_contact") + ";");
+                    if(rs2.next()){
+                        ShowPanel(new FrmContactoModelo(rs.getString("name") + " " + rs.getString("last_name"), rs2.getString("number"),rs.getString("id_contact"), rs2.getString("id_number"), usuario, this), 0, 100 * cont);
+                    }
+                    else ShowPanel(new FrmContactoModelo(rs.getString("name") + " " + rs.getString("last_name"), rs2.getString("number"),rs.getString("id_contact"), rs2.getString("id_number"), usuario, this), 0, 100 * cont);
+                    rs2.close();
+                    stmt2.close();
+                    cont++;      
                 }
-                else ShowPanel(new FrmContactoModelo(rs.getString("name") + " " + rs.getString("last_name"), rs2.getString("number"),rs.getString("id_contact"), rs2.getString("id_number"), usuario, this), 0, 100 * cont);
-                rs2.close();
-                stmt2.close();
-                cont++;      
+                ShowPanel(new JPanel(), 0, 100 * cont);
+                MainPane.setPreferredSize(new Dimension(700, 100 * cont));
+                CantidadContactos();
+            }else {
+                ListarUsuarios();
             }
-            ShowPanel(new JPanel(), 0, 100 * cont);
-            MainPane.setPreferredSize(new Dimension(700, 100 * cont));
-            CantidadContactos();
         }catch(SQLException e){
             System.out.println(e);
         }
