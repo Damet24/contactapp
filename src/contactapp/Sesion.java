45f56a68776a9736ebc1747c9f6e89986039aabb
Deivis <delahozdeivis123@gmail.com>
2021-12-01 12:20:27 -0500

    reparando la cagada :c

--- src/contactapp/Sesion.java
+++ src/contactapp/Sesion.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+// uwu
+
 package contactapp;
 
-/**
- *
- * @author danie
- */
 public class Sesion {
     private static Usuario usuario;
     
